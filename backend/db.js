47ee030d3c1974e49bdf41363b013b2015cfe172
TirthGoswami.608 <tirthgoswami.608@gmail.com>
2025-12-06 13:59:39 +0530

    create backend and fornted folder and add database connection

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+const mysql =require("mysql2");// this loads the My sql library 
+const pool= mysql.createPool({
+    host:"localhost",
+    user:"root",
+    password:"1234",
+    database:"portfolio_db",
+    waitForConnections:true,
+    connectionLimit:10,
+    queueLimit:0
+});
+module.exports=pool;
---
+++ backend/server.js
@@ -0,0 +1,4 @@
+const express =required ("express");// loads Express library
+const cors=require("cors");//cors allows your React frontend to request data from backend
+const app=express();
+
